27dac903777e6ce9f25d4de3a2df9d9b3fcf9af7
Indutnj <122307911+Indutnj@users.noreply.github.com>
2023-06-06 01:44:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./component/Dashboard";
+
+
+
+function App() {
+  return (
+   <> 
+    <div id="wrapper">
+      <Sidebar/>
+      <Dashboard/>
+    </div>
+   </>
+  );
+}
+
+export default App;
